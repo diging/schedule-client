f0da3f89e9ce4c5ff6dde2ea9ed5e4673eeb4aa5
adhvikmadhav <adhvikmadhav97@gmail.com>
2021-03-10 12:52:21 -0700

    nav-drawer

--- src/App.vue
+++ src/App.vue
@@ -9,10 +9,12 @@
 <script lang="ts">
 import router from '@/router';
 import Vue from 'vue';
-//import Footer from './components/global/Footer.vue';
+import Vuetify from 'vuetify/lib'
+import '@mdi/font/css/materialdesignicons.css'
 import Header from './components/global/Header.vue';
-//import '@fortawesome/fontawesome-free/css/all.css';
-//import '@fortawesome/fontawesome-free/js/all.js';
+
+
+
 export default Vue.extend({
 	name: 'App',
 	components: {
@@ -21,43 +23,14 @@ export default Vue.extend({
 	},
 	data: () => ({
 	}),
-	// beforeCreate() {
-	// 	/**
-	// 	 * run verify on pages other than signup and home during app setup
-	// 	 */
-	// 	if (this.$route.path !== '/signup' && this.$route.path !== '/' && this.$route.path !== '/about') {
-	// 		Vue.$verify(router, true);
-	// 	} else {
-	// 		Vue.$verify(router, false);
-	// 	}
-	// },
-	// created() {
-	// 	/**
-	// 	 * run verify on pages after load
-	// 	 * @param {Route} to - route that page is directing to
-	// 	 * @param {Route} from - route that page is directed from
-	// 	 * @param {Function} next - move on to the next hook in the pipeline.
-	// 	 * 		If no hooks are left, the navigation is confirmed.
-	// 	 */
-	// 	router.beforeEach((to, from, next) => {
-	// 		if (to.path === '/signup' || to.path === '/' || to.path === '/about') {
-	// 			Vue.$verify(router, false);
-	// 			next();
-	// 		} else {
-	// 			Vue.$verify(router, true);
-	// 			next();
-	// 		}
-	// 	});
-	// },
+	
 });
 </script>
 <style lang="scss">
 #app {
-	font-family: "Avenir", Helvetica, Arial, sans-serif;
+	font-family: "Roboto",  sans-serif;
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
 }
 #nav {
 	padding: 30px;
@@ -69,4 +42,6 @@ export default Vue.extend({
 		}
 	}
 }
+
+
 </style>
--- src/components/global/Header.vue
+++ src/components/global/Header.vue
@@ -1,34 +1,25 @@
 <template lang="pug">
-	v-app-bar(app fixed elevate-on-scroll)
-		v-toolbar-title(class="ml-4") Sham
-		v-menu(class="ml-3" offset-y open-on-hover)
-			template(v-slot:activator="{ on }")
-				v-btn(text v-on="on") Annotate
-					v-icon mdi-menu-down
-			v-list
-				v-list-item(v-for="item in annotate_items" :key="item.title" v-bind:to="item.link")
-					v-list-item-title(v-text="item.title")
-		v-btn(text large to="/transactions" class="subheading font-weight-medium") Transactions
-		v-btn(text large to="/about" class="subheading font-weight-medium") About
-		v-menu(offset-y open-on-hover)
-			template(v-slot:activator="{ on }")
-				v-btn(text v-on="on") Data
-					v-icon mdi-menu-down
-			v-list
-				v-list-item(v-for="item in data_items" :key="item.title" @click="")
-					v-list-item-title(v-text="item.title")
-		v-spacer
-		v-toolbar-items.hidden-sm-and-down
-			v-btn(text v-if="!this.$store.getters.loggedIn" @click="login") Login
-			v-btn(text v-if="!this.$store.getters.loggedIn" @click="signup") Sign Up
-			v-btn(text v-if="this.$store.getters.loggedIn" @click="logout") Log Out
-			v-btn(text) Link Two
-			v-btn(text) Link 3
-			
+	div
+		v-toolbar(app clipped-left elevate-on-scroll)
+			v-toolbar-title(class="ml-4") DIGING Scheduler
+			v-spacer
+			v-btn(color="#F2594B" class="white--text" medium v-if="!this.$store.getters.loggedIn" @click="signup") Sign In
+			v-btn(text v-if="this.$store.getters.loggedIn" @click="logout") Sign Out	
+
+		v-navigation-drawer(clipped permanent app class="mt-16")
+			v-list(dense nav)
+				v-list-item(v-for="item in items" :key="item.title" link)
+					v-list-item-icon
+						v-icon {{ item.icon }}
+					v-list-item-content
+						v-list-item-title {{ item.title }}
+
+				
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import '@mdi/font/css/materialdesignicons.css'
 export default Vue.extend({
 	name: 'Header',
 	props: {
@@ -36,39 +27,21 @@ export default Vue.extend({
 	},
 	data() {
 		return {
-			activeIndex: '1',
-			annotate_items: [{ title: 'Text', link: '/text' }, { title: 'Projects', link: '/project' }],
-			data_items: [{ title: 'Concepts' }, { title: 'Annotations' }],
-			info_items: [
-				{ title: 'Overview' },
-				{ title: 'Use Cases' },
-				{ title: 'Our Team' },
-			],
+			items: [
+				{ title: 'Worker Schedule', icon: 'mdi-clock-time-five-outline'},
+				{ title: 'Leave Requests', icon: 'mdi-list-status'},
+				{ title: 'Your Schedule', icon: 'mdi-briefcase'},
+			]			
 		};
-  },
+  	},
 	methods: {
-		handleSelect(key: string, keyPath: string[]) {
-			this.activeIndex = key;
-		},
-		pushHome() {
-			this.$router.push('/');
-		},
-		login() {
-			this.$router.push('login');
-		},
-		signup() {
-			this.$router.push('signup');
-		},
-		logout() {
-			localStorage.removeItem('token');
-			this.login();
-		},
+		
 	},
 });
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
+<style scoped>
 li a {
 	color: inherit; /* blue colors for links too */
 	text-decoration: inherit; /* no underline */
