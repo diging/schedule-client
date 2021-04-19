b976fb7b498dd0cbcff4e008e19bfb5b4b1724e2
adhvikmadhav <adhvikmadhav97@gmail.com>
2021-04-19 10:19:35 -0700

    password input

--- src/components/auth/Signin.vue
+++ src/components/auth/Signin.vue
@@ -3,7 +3,10 @@
         h2(class="mb-8") Welcome.
         v-form(ref="form")
             v-text-field(v-model="email" label="ASU Email" required)
-            v-text-field(class="mb-5" v-model="password" label="Password" required)
+            v-text-field(class="mb-5" v-model="password" label="Password" required
+                :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'" @click:append="show1 = !show1"
+                :rules="[rules.required, rules.min]" counter
+                :type="show1 ? 'text' : 'password'")
             v-btn(color="#F2594B" class="white--text mb-5" medium @click="login()") Sign In
             v-btn(plain medium to="createAccount") Create Account
 
@@ -16,6 +19,17 @@ import Component from 'vue-class-component';
 
 @Component({
 	name: 'Signin',
+    data() {
+        return {
+            show1: false,
+            password: '',
+            rules: {
+                required: (value: any) => !!value || 'Required.',
+                min: (v: string|any[]) => v.length >= 8 || 'Min 8 characters',
+                emailMatch: () => (`The email and password you entered don't match`),
+            }
+        }
+    }
 })
 
 export default class Signin extends Vue{
