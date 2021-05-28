b6363b232544114442e415432738181dc3416058
tjquinn1 <tjquinn1@asu.edu>
2021-05-27 17:09:25 -0700

    added jwt decode

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,21 @@ import store from './store'
 import vuetify from './plugins/vuetify';
 import '@babel/polyfill'
 import 'material-design-icons-iconfont/dist/material-design-icons.css';
+import { abilitiesPlugin, Can } from '@casl/vue';
+import { buildAbilityFor } from './config/ability';
+
+
+const ability = buildAbilityFor('member');
+
+if (process.env.NODE_ENV !== 'production') {
+	// exposed for debugging
+	(window as any).ability = ability
+  }
+
 
 Vue.config.productionTip = false
+Vue.use(abilitiesPlugin, ability);
+Vue.component('Can', Can);
 
 new Vue({
   router,
